797f82f67b4dc9816ed37f2111bbd58a7a681310
Edward Andress <eddie_andress@hotmail.com>
2020-11-06 11:31:40 +0000

    LONG shot

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -4,8 +4,8 @@ require 'pg'
 
 def setup_test_database
   p 'Setting up test database ... '
-  connection = PG.connect(dbname: 'bookmark_manager_test')
-
+  connection = PG.connect(dbname: "bookmark_manager_test")
+  
   connection.exec('TRUNCATE bookmarks;')
   p 'Completed setup ... '
 end
