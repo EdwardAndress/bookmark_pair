cdd8a61cea0ad94f367a4182328fad33106cf8bb
Edward Andress <eddie_andress@hotmail.com>
2020-11-26 12:34:55 +0000

    my awesome commit

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -5,7 +5,7 @@ require 'pg'
 def setup_test_database
   p 'Setting up test database ... '
   connection = PG.connect(dbname: "bookmark_manager_test")
-  
+  p connection.exec('\dt')
   connection.exec('TRUNCATE bookmarks;')
   p 'Completed setup ... '
 end
